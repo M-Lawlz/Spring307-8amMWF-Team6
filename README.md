846b19e98fe09ced9a508bfbf395ed83d092a0dc
Matthew Lawler <matthewlawler99@gmail.com>
2020-05-03 15:10:47 -0700

    Formatting Project with Prettier

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Spring307-8amMWF-Team6
 
 # Project Idea: SimTrek
+
 ## Team Members: Nitish Padavala, Matthew Lawler, Wilson Huang
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
     </div>
   );
 }
