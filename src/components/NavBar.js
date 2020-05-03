20f262e61878231e495b902328cdd7490c6e0ea7
Matthew Lawler <matthewlawler99@gmail.com>
2020-05-03 13:30:59 -0700

    Implementing Login

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,57 +1,124 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import SignUp from './SignUp.js';
-import Login from './Login.js';
+import App from "firebase/app";
+import "firebase/auth";
+import {
+  AppBar,
+  Button,
+  Grid,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import Login from "./Login.js";
+import React from "react";
+import SignUp from "./SignUp.js";
 
 export default class NavBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showLogin : false,
-            showSignUp : false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      isLoginShowing: false,
+      isSignUpShowing: false,
+      user: null,
+    };
+  }
 
-    handleLoginClicked = (newVal) => {
-        this.setState({
-            showLogin: newVal
-        });
-    }
+  attemptLogout = () => {
+    App.auth().signOut().then(this.logout).catch(this.handleError);
+  };
 
-    handleSignUpClicked = (newVal) => {
-        this.setState({
-            showSignUp: newVal
-        });
-    }
+  componentDidMount() {
+    App.auth().onAuthStateChanged((user) => {
+      this.setState({ isLoading: false, user: user });
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <AppBar position="static" style={{background: '#00CED1'}}>
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" aria-label="menu">
-                        </IconButton>
-                        <Typography variant="h6">
-                            <h2>SimTrek</h2>
-                        </Typography>
-                        <Grid container alignItems="flex-start" justify="flex-end" direction="row">
-                            <Button color="inherit" variant="outlined" type="submit" onClick={this.handleLoginClicked}>                         
-                            <em>Login</em>
-                            </Button> &nbsp;
-                            {this.state.showLogin ? <Login loginAttempt={this.handleLoginClicked}/> : null}
-                            <Button color="inherit" variant="outlined" type="submit" onClick={this.handleSignUpClicked}>
-                                <em>Sign Up</em>
-                            </Button>
-                            {this.state.showSignUp ? <SignUp signupAttempt={this.handleSignUpClicked}/> : null}
-                        </Grid>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        )
+  handleError = (error) => {
+    alert(error.message);
+  };
+
+  loginClicked = (newVal) => {
+    this.setState({
+      isLoginShowing: newVal,
+    });
+  };
+
+  logout = () => {
+    alert("You have been logged out of SimTrek.");
+    this.setState({
+      user: null,
+    });
+  };
+
+  signUpClicked = (newVal) => {
+    this.setState({
+      isSignUpShowing: newVal,
+    });
+  };
+
+  render() {
+    if (this.state.isLoading) {
+      return null;
+    } else {
+      return (
+        <div>
+          <AppBar position="static" style={{ background: "#00CED1" }}>
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+              ></IconButton>
+              <Typography variant="h6">
+                <h2>SimTrek</h2>
+              </Typography>
+              <Grid
+                container
+                alignItems="flex-start"
+                justify="flex-end"
+                direction="row"
+              >
+                {this.state.user ? null : (
+                  <Button
+                    color="inherit"
+                    variant="outlined"
+                    type="submit"
+                    onClick={this.loginClicked}
+                  >
+                    <em>Login</em>
+                  </Button>
+                )}
+                &nbsp;
+                {this.state.isLoginShowing ? (
+                  <Login loginShowing={this.loginClicked} />
+                ) : null}
+                {this.state.user ? (
+                  <Button
+                    color="inherit"
+                    variant="outlined"
+                    type="submit"
+                    onClick={this.attemptLogout}
+                  >
+                    <em>Log Out</em>
+                  </Button>
+                ) : (
+                  <Button
+                    color="inherit"
+                    variant="outlined"
+                    type="submit"
+                    onClick={this.signUpClicked}
+                  >
+                    <em>Sign Up</em>
+                  </Button>
+                )}
+                {this.state.isSignUpShowing ? (
+                  <SignUp signUpShowing={this.signUpClicked} />
+                ) : null}
+              </Grid>
+            </Toolbar>
+          </AppBar>
+        </div>
+      );
     }
+  }
 }
