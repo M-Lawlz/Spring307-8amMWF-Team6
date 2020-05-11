f773c37cc734b39b255ac094a34a05b844076059
Nitish Padavala <padavala.nitish@gmail.com>
2020-05-10 18:04:29 -0700

    App.js

--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,45 @@
 import React from "react";
+import "./App.css"
+import TourSample from './TourSample'
+import "firebase/firestore";
+
 
 export default class Tours extends React.Component {
-  render() {
-    return <h1>Tours</h1>;
+ constructor(props) {
+       super(props);
+       this.state = {
+           tours : [],
+           isSearching : false,
+           selectDropdowns : [],
+           searchVal : "",
+           retrieveTourPage : false,
+           currentTour : []
+       }
+   }
+  
+ 
+   componentDidMount() {
+       const firebase = require("firebase");
+       const db = firebase.firestore();
+       const toursDb = db.collection("Tours").get();
+ 		var dbTourArray = this.state.tours;
+       
+   }
+
+
+render() {
+   return (<div className="container">
+      <h1>View Tours</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <dbTourArray />
+        </div>
+      </div>
+    </div>) 
   }
-}
+
+  	}
+  
+
+  
+
