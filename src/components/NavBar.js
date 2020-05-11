a76760d1588cc3146165fb770f8264176a78bd15
Nitish Padavala <padavala.nitish@gmail.com>
2020-05-10 18:07:23 -0700

    components/NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -87,6 +87,11 @@ export default class NavBar extends React.Component {
                     <h3>Tours</h3>
                   </Button>
                 </Link>
+                <Link style={{ color: "inherit" }} to={"/UploadTour"}>
+                  <Button color={"inherit"} style={{ marginLeft: 35, width: 100}}>
+                    <h3>Upload a Tour</h3>
+                  </Button>
+                </Link>
               </HashRouter>
               <Grid
                 container
