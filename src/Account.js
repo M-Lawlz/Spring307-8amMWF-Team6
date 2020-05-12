b9f3c389170c15f64725ab455a61d4d6916819a5
Matthew Lawler <matthewlawler99@gmail.com>
2020-05-11 22:22:53 -0700

    Fetching user info from database to display on account page.

--- src/Account.js
+++ src/Account.js
@@ -1,7 +1,51 @@
+import App from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 import React from "react";
 
 export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      userData: null,
+    };
+  }
+
+  fetchUserInfo = () => {
+    const userDoc = App.firestore()
+      .collection("users")
+      .doc(this.state.user.email);
+    userDoc
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          this.setState({ userData: doc.data() });
+        }
+      })
+      .catch((error) => {
+        console.error("Error: ", error.message);
+      });
+  };
+
+  async componentDidMount() {
+    await App.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user: user });
+        this.fetchUserInfo();
+      }
+    });
+  }
+
   render() {
-    return <h1>Account</h1>;
+    if (!this.state.userData) {
+      return null;
+    } else {
+      return (
+        <div>
+          <h1>Welcome, {this.state.userData.username}!</h1>
+        </div>
+      );
+    }
   }
 }
