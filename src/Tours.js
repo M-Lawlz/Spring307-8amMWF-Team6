d4cef9e5db602b7c197e66db95dc215b04234684
Nitish Padavala <padavala.nitish@gmail.com>
2020-05-11 19:41:52 -0700

    console logs

--- src/Tours.js
+++ src/Tours.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React,{useState}  from "react";
 import "./App.css"
 import TourSample from './TourSample'
 import "firebase/firestore";
 
 
 export default class Tours extends React.Component {
- constructor(props) {
+ 	constructor(props) {
        super(props);
-       this.state = {
-           tours : [],
+
+
+   this.state = {
+           tours : [{}],
            isSearching : false,
            selectDropdowns : [],
            searchVal : "",
@@ -16,25 +18,49 @@ export default class Tours extends React.Component {
            currentTour : []
        }
    }
+
   
+   handleNewData = (tour) => {
+       var dbTourArray = this.state.tours;
+       var newDbTour = {tourId: tour.data().tourId,
+                   userEmail: tour.data().userEmail,
+                   location: tour.data().location,
+                   uploadDate: tour.data().uploadDate,
+                   videoUrl: tour.data().videoUrl,
+                   description: tour.data().description};
+       dbTourArray.push(newDbTour);
+      console.log(this.state.tours)
+       this.setState({
+
+           tours : dbTourArray
+       
+       });
+   }
  
    componentDidMount() {
+   		
        const firebase = require("firebase");
        const db = firebase.firestore();
-       const toursDb = db.collection("Tours").get();
- 		var dbTourArray = this.state.tours;
-       
+       const toursDb = db.collection("Tours");
+ 
+       toursDb.get().then(snapshot => {
+           snapshot.forEach(doc => {
+               this.handleNewData(doc);
+           });
+       });
    }
+   
 
 
 render() {
+	const t = this.state.tours[0]
+	const a = t.TourId
+	console.log(t)
+	console.log(a)
+	const tours = this.state.tours
    return (<div className="container">
       <h1>View Tours</h1>
-      <div className="flex-row">
-        <div className="flex-large">
-          <dbTourArray />
-        </div>
-      </div>
+      	
     </div>) 
   }
 
