17f278981558f0c31904ee6eba9759ea2d21a53b
Nitish Padavala <padavala.nitish@gmail.com>
2020-06-04 23:57:54 -0700

    Update UploadTourTests.js

--- src/__tests__/UploadTourTests.js
+++ src/__tests__/UploadTourTests.js
@@ -2,8 +2,12 @@ import React from "react";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import UploadTour from "../UploadTour";
+import AddTourForm from "../AddTourForm";
+
 
 test("UploadTour renders successfully", () => {
   const uploadTour = shallow(<UploadTour />);
   expect(toJson(uploadTour)).toMatchSnapshot();
 });
+
+
