1c56cac2e131d9f37943ea75beadc8fdade98ebf
Nitish Padavala <padavala.nitish@gmail.com>
2020-06-04 23:57:17 -0700

    Update AddTourFormTests.js

--- src/__tests__/AddTourFormTests.js
+++ src/__tests__/AddTourFormTests.js
@@ -1,9 +1,28 @@
 import AddTourForm from "../AddTourForm";
 import React from "react";
-import { shallow } from "enzyme";
+import { render } from "enzyme";
 import toJson from "enzyme-to-json";
+import { shallow } from "enzyme";
+
 
 test("AddTourForm renders successfully.", () => {
-  const addTourForm = shallow(<AddTourForm />);
+  const addTourForm = render(<AddTourForm />);
   expect(toJson(addTourForm)).toMatchSnapshot();
+});
+
+const wrapper = shallow(<AddTourForm />);
+describe("updateInput", () => {
+  it("should call setState", () => {
+    const expected = {
+      description: "test",
+      location: "test",
+      uploadDate: "test"
+    
+    };
+    
+    setTimeout(() => {
+    wrapper.instance().updateInput({target: { value: 'test', name: 'test'}});
+    expect(wrapper.state.test).toEqual('test');
+  }, 0);
+  });
 });
